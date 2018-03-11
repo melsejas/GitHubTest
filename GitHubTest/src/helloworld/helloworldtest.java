804d322b6db75414252bf4d093ab06062b974019
admiralcoder <djale2008@icloud.com>
2018-03-11 13:46:39 -0400

    Worked on melisas project

--- GitHubTest/src/helloworld/helloworldtest.java
+++ GitHubTest/src/helloworld/helloworldtest.java
@@ -5,6 +5,7 @@ public class helloworldtest {
 	public static void main(String[] args) {
 		System.out.println("My name is Minnie!");
 		System.out.println("My name is Mickey!");
+		System.out.println("my name is Alex i am IT proffetional ");
 
 	}
 
